949c4d007bf156280cac9455baa791330940dd77
Audrey Pachol <audrey.pachol@gmail.com>
2023-07-24 18:19:09 +0200

    update
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/audreypchl/code/audreypa/cpp/Finance-lib
-# Build directory: /Users/audreypchl/code/audreypa/cpp/Finance-lib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/audreypchl/code/audreypa/cpp/Finance-lib/CMakeLists.txt;17;add_test;/Users/audreypchl/code/audreypa/cpp/Finance-lib/CMakeLists.txt;0;")
